2f82556c9fbca05d396098f4781aab9608fee421
Hillel Saal <remi.saal@gmail.com>
2023-07-31 09:24:34 +0300

    feat: Initial commit

---
+++ src/main/java/com/example/criteointerview/CriteoInterviewApplication.java
@@ -0,0 +1,11 @@
+package com.example.criteointerview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriteoInterviewApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CriteoInterviewApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/criteointerview/CriteoInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.criteointerview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriteoInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
